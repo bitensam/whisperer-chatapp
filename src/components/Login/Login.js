6076ea388a10e88d67d303f40d6f19c511d62acd
Kuba Amber <kuba@ambergrupa.pl>
2021-08-20 13:53:48 +0200

    Add login page startup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,22 @@
-import React from "react";
-import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
+import React from 'react';
+import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div id="login-page" className="login-page">
+      <div id="login-card" className="login-card">
+        <h2>Welcome to Whisperer</h2>
+        <div className="login-button google">
+          <GoogleOutlined/> Sign In with Google
+        </div>
+        <br />
+        <br />
+        <div className="login-button facebook">
+          <FacebookOutlined/> Sign In with Facebook
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
